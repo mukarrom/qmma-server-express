5425e7198be5860a68a229f66c302a7f81e686ea
mukarrom21 <mukarrom21@gmail.com>
2024-07-06 19:29:40 +0600

    route added

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,12 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 // Routes
-// app.use("/api/", router);
+app.use("/api/", routes_1.default);
 app.get("/", (req, res) => {
     res.send("Welcome to Qawmi management server app!");
 });
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import notFound from "./app/middlewares/notFound";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Routes
-// app.use("/api/", router);
+app.use("/api/", router);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Qawmi management server app!");
